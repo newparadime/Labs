3002da5fcd8614c184b8efca50b4a9128a8ec3ac
newparadime <bergproductions@gmail.com>
2015-02-13 15:05:26 -0500

    Finished Lab 1

--- LesebergV-Lab1/Functions.h
+++ LesebergV-Lab1/Functions.h
@@ -1,19 +1,28 @@
 using namespace std;
 
+//------------
+// Greatest Common Denominator Function
 int GCD(int a, int b)
 {
+	//First base case if both integers are zero
 	if (a + b == 0)
 	{
 		return 0;
 	}
+
+	//Second base case if either integer is not zero
 	else if (a == 0)
 	{
 		return b;
 	}
+	
+	//Case for two even integers
 	else if (a % 2 + b % 2 == 0)
 	{
 		return 2 * GCD(a / 2, b / 2);
 	}
+
+	//Case for two odd integers
 	else if (a % 2 * b % 2 != 0)
 	{
 		if (a >= b)
@@ -26,38 +35,107 @@ int GCD(int a, int b)
 			return GCD((b - a) / 2, a);
 		}
 	}
+
+	//Case if b is odd and a is even
 	else if (a % 2 == 0)
 	{
 		
 		return GCD(a / 2, b);
 	}
+	
+	//Case if a is odd and b is even
 	else
 	{
 		return GCD(a, b / 2);
 	}
 }
 
-double exp(double y, int x, int base = 0)
+//-----------------
+// Function to raise y^x to y^x
+double ExpBig(double y, int x, int base = 0)
 {
+	//Sets the initial base 'y' equal to 'base'
 	if (base == 0) base = static_cast<int>(y);
 
+	//Base case for when the ExpBigonent is 1
+	//	(When counting up from ExpBigonent * -1)
 	if (x == -1)
 	{
 		return y;
 	}
+
+	//Case reached when the initial value of y^x is reached
 	else if (x == 1)
 	{
-		cout << y;
-		return exp(y, 0-static_cast<int>(y));
+		return ExpBig(y, 0 - static_cast<int>(y));
 	}
+
+	//Case when the ExpBigonent of the initial y^x calculation is 
+	//	greater than one
 	else if (x > 1)
 	{
-		cout << y << endl << x << endl;
-		return exp(y * base, x - 1, base);
+		return ExpBig(y * base, x - 1, base);
 	}
+
+	//Case when the ExpBigonent of the second y^x to the y^x
+	//	calculation is greater than 1
 	else
 	{
-		cout << y << endl << x << endl;
-		return exp(y * base, x + 1, base);
+		return ExpBig(y * base, x + 1, base);
+	}
+}
+
+////-----------------
+//// Function to raise y to the x (int overload)
+//int exp(int y, int x)
+//{
+//	//Case reached when the value of y^x is reached (exp = 1)
+//	if (x == 1)
+//	{
+//		return y;
+//	}
+//
+//	//Case when the exponent of the initial y^x calculation is 
+//	//	greater than one
+//	else
+//	{
+//		return y * exp(y, x - 1);
+//	}
+//}
+//
+////-----------------
+//// Function to raise y to the x (Double overload)
+//double exp(double y, int x)
+//{
+//	//Case reached when the value of y^x is reached (exp = 1)
+//	if (x == 1)
+//	{
+//		return y;
+//	}
+//
+//	//Case when the exponent of the initial y^x calculation is 
+//	//	greater than one
+//	else
+//	{
+//		return y * exp(y, x - 1);
+//	}
+//}
+
+//-----------------
+// Function to raise y to the x (Template Class)
+template <class T>
+T exp(T y, int x)
+{
+	//Case reached when the value of y^x is reached (exp = 1)
+	if (x == 1)
+	{
+		return y;
+	}
+
+	//Case when the exponent of the initial y^x calculation is 
+	//	greater than one
+	else
+	{
+		return y * exp(y, x - 1);
 	}
 }
--- LesebergV-Lab1/Lab1.cpp
+++ LesebergV-Lab1/Lab1.cpp
@@ -11,18 +11,57 @@ using namespace std;
 
 int main(void)
 {
-	//int a;
-	//int b;
-	//cin >> a;
-	//cin >> b;
-	//cout << GCD(a, b);
+	int a;
+	int b;
+
+	//Tests the GCD function with two integers
+	cout << "This function calculates the GCD"
+		 << " for two integer numbers.\n\n"
+		 << "Please enter an integer 'a': ";
+	cin >> a;
+	cout << "\nPlease enter and integer 'b': ";
+	cin >> b; 
+	cout << "The GCD of 'a' and 'b' is: " << GCD(a, b)
+		<< "\n\n\n";
+
 	int x;
 	int y;
-	int z = 1;
-	while (z != 0)
-	{
-		cin >> z >> x >> y;
-		cout << exp(x, y);
-	}
+
+	//Tests exp function with an integer base
+	//	and integer exponent
+	cout << "This function calculates y^x\n\n"
+		 << "Please enter an integer 'y':";
+	cin >> y;
+	cout << "\nPlease enter and integer 'x':";
+	cin >> x;
+	cout << "\n'y' raised to the power 'x' is: " << exp(y, x)
+		<< "\n\n\n";
+
+	double yFloat;
+
+	//Tests exp function with a floating point base
+	//	and integer exponent
+	cout << "This function calculates y^x\n\n"
+		 << "Please enter a floating point number 'y': ";
+	cin >> yFloat;
+	cout << "\nPlease enter an integer 'x': ";
+	cin >> x;
+	cout << "\n'y' raised to the power 'x' is: " << exp(yFloat, x)
+		 << "\n\n\n";
+
+	//This function calculates y raised to the x then raised to itself
+	cout << "This function calculates y^x raised to y^x\n\n"
+		<< "Please enter an integer 'y': ";
+	cin >> yFloat;
+	cout << "\nPlease enter an integer 'x': ";
+	cin >> x;
+	
+	//Note a double is used in this function not because
+	//	a floating point is needed, but because the values
+	//	get very large and an int cannot store the data
+	cout << "\n'y^x' raised to the power 'y^x' is: " << ExpBig(yFloat, x)
+		<< "\n\n";
+
 	return 0;
 }
+
