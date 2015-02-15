77af68d7fdf3f4995d1ca989804397460470a18c
newparadime <bergproductions@gmail.com>
2015-02-15 17:43:19 -0500

    Code Complete, needs commenting

--- LesebergV-Lab2/Lab2.cpp
+++ LesebergV-Lab2/Lab2.cpp
@@ -12,32 +12,58 @@
 
 using namespace std;
 
-enum player { red = 'R', yellow = 'Y', proceed = '-' };
-const int ARRAY_SIZE = 4;
+typedef enum { red = 'R', yellow = 'Y', proceed = '-', draw } player;
+const int ARRAY_SIZE = 10;
+const int COLUMN_WIDTH = 4;
 
 void GetFirstPlayer(player &firstPlayer, player &secondPlayer);
 void CreateBoard(player matrix[][ARRAY_SIZE]);
 void GetNextMove(player matrix[][ARRAY_SIZE], int height[ARRAY_SIZE], player redOrYellow);
 void PrintBoard(player matrix[][ARRAY_SIZE]);
 player Winner(player matrix[][ARRAY_SIZE]);
+player CheckRow(player matrix[][ARRAY_SIZE]);
+player CheckCollumn(player matrix[][ARRAY_SIZE]);
+player CheckDiagonalForward(player matrix[][ARRAY_SIZE], int j = 0, int i = 0, int counter = 0);
+player CheckDiagonalBackward(player matrix[][ARRAY_SIZE], int j = 0, int i = ARRAY_SIZE - 1, int counter = 0);
+player CheckDraw(player matrix[][ARRAY_SIZE]);
+player CheckRowInternal(player matrix[][ARRAY_SIZE], int j, int i = 0, int counter = 0);
+player CheckCollumnInternal(player matrix[][ARRAY_SIZE], int i, int j = 0, int counter = 0);
 
 int main(void)
 {
-	cout << red << "test";
 	player firstPlayer;
 	player secondPlayer;
+	player winner = proceed;
 	player board[ARRAY_SIZE][ARRAY_SIZE];
 
-	int height[ARRAY_SIZE]{0, 0, 0};
+	int height[ARRAY_SIZE]{0, 0, 0, 0};
 	
 	GetFirstPlayer(firstPlayer, secondPlayer);
 
 	CreateBoard(board);
+	PrintBoard(board);
 
-	while (Winner(board) == proceed)
+	while (winner == proceed)
 	{
+		GetNextMove(board, height, firstPlayer);
 		PrintBoard(board);
-		GetNextMove(board, height, redOrYellow);
+		winner = Winner(board);
+
+		if (winner != firstPlayer)
+		{
+			GetNextMove(board, height, secondPlayer);
+			PrintBoard(board);
+			winner = Winner(board);
+		}
+	}
+
+	if (winner == draw)
+	{
+		cout << "The game was a draw";
+	}
+	else
+	{
+		cout << static_cast<char>(winner) << " wins!!";
 	}
 
 	return 0;
@@ -47,10 +73,12 @@ void GetFirstPlayer(player &firstPlayer, player &secondPlayer)
 {
 	char inputPlayer;
 
-	cout << "This program alows to people to play the game ConnectFour. \n"
+	cout << "This program alows people to play the game ConnectFour. \n\n"
 		<< "First player, Please enter 'Y' for Yellow or 'R' for Red: ";
 
 	cin >> inputPlayer;
+
+	inputPlayer = static_cast<char>(toupper(inputPlayer));
 
 	cout << endl << endl;
 
@@ -73,7 +101,7 @@ void CreateBoard(player matrix[][ARRAY_SIZE])
 	{
 		for (int j = 0; j < ARRAY_SIZE; j++)
 		{
-			matrix[i][j] = proceed;
+			matrix[j][i] = proceed;
 		}
 	}
 }
@@ -81,72 +109,258 @@ void CreateBoard(player matrix[][ARRAY_SIZE])
 void GetNextMove(player matrix[][ARRAY_SIZE], int height[], player redOrYellow)
 {
 	int move;
-	int positionX;
-	int positionY;
+	int collumn;
+	int row;
 
-	cout << "Please select a column to make a move: ";
+	cout << "Player \'" << static_cast<char>(redOrYellow) << "\' "
+		 << "Please select a column to make a move: ";
 
 	cin >> move;
 
 	cout << endl << endl;
 
-	positionX = move;
-	positionY = height[move];
+	collumn = move - 1;
+	row = height[collumn];
 	
-	matrix[positionX][positionY] = redOrYellow;
-	height[move]++;
+	matrix[row][collumn] = redOrYellow;
+	height[collumn]++;
 }
 
 void PrintBoard(player matrix[][ARRAY_SIZE])
 {
-	for (int i = 0; i < 4; i++)
+	for (int counter = 1; counter <= ARRAY_SIZE; counter++)
 	{
-		cout << "---------------------\n";
-		for (int j = 0; j < 4; j++)
+		cout << setw(COLUMN_WIDTH - 1) << counter << " ";
+	}
+	cout << "\n";
+
+	for (int i = ARRAY_SIZE - 1; i >= 0; i--)
+	{
+		for (int counter = 0; counter < ARRAY_SIZE * COLUMN_WIDTH; counter++)
 		{
-			cout << "| " << setw(2) << matrix[i][j] << setw(1) << " ";
+			cout << "-";
+		}
+		cout << "-\n";
+		for (int j = 0; j < ARRAY_SIZE; j++)
+		{
+			cout << "| " << setw(COLUMN_WIDTH - 3)
+				 << static_cast<char>(matrix[i][j]) << " ";
 		}
 		cout << "|\n";
 	}
-	cout << "---------------------";
+	for (int counter = 0; counter < ARRAY_SIZE * COLUMN_WIDTH; counter++)
+	{
+		cout << "-";
+	}
+	cout << "-\n\n\n";
 }
 
 player Winner(player matrix[][ARRAY_SIZE])
 {
-	player winner;
+	player winner = proceed;
+	
+	winner = CheckRow(matrix);
+
+	if (winner == proceed)
+	{
+		winner = CheckCollumn(matrix);
+
+		if (winner == proceed)
+		{
+			winner = CheckDiagonalForward(matrix);
+
+			if (winner == proceed)
+			{
+				winner = CheckDiagonalBackward(matrix);
+
+				if (winner == proceed)
+				{
+					CheckDraw(matrix);
+				}
+			}
+		}
+	}
+	
+	return winner;
+}
+
+player CheckRow(player matrix[][ARRAY_SIZE])
+{
+	player winner = proceed;
+
 	for (int j = 0; j < ARRAY_SIZE; j++)
 	{
-		int i;
-		for (i = 0; i < ARRAY_SIZE - 1 && matrix[i][j] == matrix[i+1][j]; i++)
+		if (winner == proceed)
 		{
-			winner = matrix[i + 1][j];
+			winner = CheckRowInternal(matrix, j);
 		}
-
 	}
+	return winner;
+}
+
+player CheckCollumn(player matrix[][ARRAY_SIZE])
+{
+	player winner = proceed;
 
 	for (int i = 0; i < ARRAY_SIZE; i++)
 	{
-		int j;
-		for (j = 0; j < ARRAY_SIZE - 1 && matrix[i][j] == matrix[i][j+1]; j++)
+		if (winner == proceed)
 		{
-			winner = matrix[i][j+1];
+			winner = CheckCollumnInternal(matrix, i);
 		}
-
-	}
-
-	int i = 0;
-	for (int j = 0; j < ARRAY_SIZE - 1 && matrix[i][j] == matrix[i + 1][j + 1]; j++)
-	{
-		winner = matrix[i + 1][j + 1];
-		i++;
-	}
-
-	i = 3;
-	for (int j = 0; j < ARRAY_SIZE - 1 && matrix[i][j] == matrix[i - 1][j + 1]; i++)
-	{
-		winner = matrix[i - 1][j + 1];
-		i--;
 	}
 
 	return winner;
+}
+
+player CheckDraw(player matrix[][ARRAY_SIZE])
+{
+	player winner = proceed;
+	
+	if (matrix[ARRAY_SIZE - 1][0] != proceed)
+	{
+		winner = draw;
+		cout << static_cast<char>(winner);
+	}
+	return winner;
+}
+
+player CheckRowInternal(player matrix[][ARRAY_SIZE], int j, int i, int counter)
+{
+	if (counter == 3)
+	{
+		if (matrix[j][i] != proceed)
+		{
+			return matrix[j][i];
+		}
+		else
+		{
+			i++;
+			return CheckRowInternal(matrix, j, i);
+		}
+	}
+	else if (i >= ARRAY_SIZE - 1)
+	{
+		return proceed;
+	}
+	else if (matrix[j][i] == matrix[j][i + 1])
+	{
+		counter++;
+		i++;
+		return CheckRowInternal(matrix, j, i, counter);
+	}
+	else
+	{
+		i++;
+		return CheckRowInternal(matrix, j, i);
+	}
+
+}
+
+player CheckCollumnInternal(player matrix[][ARRAY_SIZE], int i, int j, int counter)
+{
+	if (counter == 3)
+	{
+		if (matrix[j][i] != proceed)
+		{
+			return matrix[j][i];
+		}
+		else
+		{
+			j++;
+			return CheckCollumnInternal(matrix, i, j);
+		}
+	}
+	
+	else if (j >= ARRAY_SIZE - 1)
+	{
+		return proceed;
+	}
+	
+	else if (matrix[j][i] == matrix[j + 1][i])
+	{
+		counter++;
+		j++;
+		return CheckCollumnInternal(matrix, i, j, counter);
+	}
+	else
+	{
+		j++;
+		return CheckCollumnInternal(matrix, i, j);
+	}
+
+}
+
+player CheckDiagonalForward(player matrix[][ARRAY_SIZE], int j, int i, int counter)
+{
+	if (counter == 3)
+	{
+		if (matrix[j][i] != proceed)
+		{
+			return matrix[j][i];
+		}
+		else
+		{
+			j++;
+			i++;
+			return CheckDiagonalForward(matrix, j, i);
+		}
+	}
+
+	else if (j >= ARRAY_SIZE - 1)
+	{
+		return proceed;
+	}
+
+	else if (matrix[j][i] == matrix[j + 1][i + 1])
+	{
+		counter++;
+		j++;
+		i++;
+		return CheckDiagonalForward(matrix, j, i, counter);
+	}
+	else
+	{
+		j++;
+		i++;
+		return CheckDiagonalForward(matrix, j, i);
+	}
+
+}
+
+player CheckDiagonalBackward(player matrix[][ARRAY_SIZE], int j, int i, int counter)
+{
+	if (counter == 3)
+	{
+		if (matrix[j][i] != proceed)
+		{
+			return matrix[j][i];
+		}
+		else
+		{
+			j++;
+			i--;
+			return CheckDiagonalBackward(matrix, j, i);
+		}
+	}
+
+	else if (j >= ARRAY_SIZE - 1)
+	{
+		return proceed;
+	}
+
+	else if (matrix[j][i] == matrix[j + 1][i - 1])
+	{
+		counter++;
+		j++;
+		i--;
+		return CheckDiagonalBackward(matrix, j, i, counter);
+	}
+	else
+	{
+		j++;
+		i--;
+		return CheckDiagonalBackward(matrix, j, i);
+	}
+
 }
